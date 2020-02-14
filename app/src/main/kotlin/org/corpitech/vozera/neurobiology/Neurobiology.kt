b55e9c052f747e0698e3351b7ec692c906a7824f
mirays <my@mirays.tech>
2020-02-14 15:17:28 +0300

    Started working on Neurobiology module

--- app/src/main/kotlin/org/corpitech/vozera/neurobiology/Neurobiology.kt
+++ app/src/main/kotlin/org/corpitech/vozera/neurobiology/Neurobiology.kt
@@ -1,4 +1,16 @@
 package org.corpitech.vozera.neurobiology
 
 class Neurobiology {
+    var Dominance = 0
+    var Sexual = 0
+    var Resource = 0
+    var Social = 0
+
+    val Value: Int
+        get() {
+            return (Dominance + Sexual + Resource + Social) * 999 / 396 // 4*99
+        }
+
+
+
 }
